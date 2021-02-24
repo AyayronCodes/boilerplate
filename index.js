e0d600a800782d0920f4ed159c66a43dbfa720d7
Aaron Han <aaron@ayayron.com>
2021-02-24 15:24:00 +0900

    Protect keys

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
+const { User } = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+// application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://aaron:12345@cluster0.rtyug.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 })
 .then(() => console.log('MongoDB Connected...'))
@@ -11,5 +21,16 @@ mongoose.connect('mongodb+srv://aaron:12345@cluster0.rtyug.mongodb.net/myFirstDa
 
 
 app.get('/', (req, res) => res.send("Hello World!"))
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  // save is mongoDB method
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
